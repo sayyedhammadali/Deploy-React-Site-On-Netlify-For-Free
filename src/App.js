cec31f0ac0f79cd242375105164217929b925c94
Hammad Ali <hammad@Hammads-MBP.fritz.box>
2021-10-02 00:58:28 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
+import ReactLogo from './react-logo.svg';
+import NetlifyLogo from './netlify-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="logo-container">
+          <img src={ReactLogo} className="App-logo" alt="logo" />
+          <img src={NetlifyLogo} className="App-logo" alt="logo" />
+        </div>
+        <h3>
+          Deploy React Site On Netlify For <span className="free">FREE</span>!!!
+        </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HOW ??? Click HERE to Checkout My Medium Article
         </a>
       </header>
     </div>
